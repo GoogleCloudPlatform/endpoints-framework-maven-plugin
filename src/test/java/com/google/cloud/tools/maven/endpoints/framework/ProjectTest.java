301636ed8f2f8db48623dadc71a6835eab8ca0bd
Appu Goundan <appu@google.com>
2017-04-26 14:08:38 -0400

    Add hostname option (#18)

--- src/test/java/com/google/cloud/tools/maven/endpoints/framework/ProjectTest.java
+++ src/test/java/com/google/cloud/tools/maven/endpoints/framework/ProjectTest.java
@@ -17,6 +17,17 @@
 
 package com.google.cloud.tools.maven.endpoints.framework;
 
+import com.google.common.base.Charsets;
+import com.google.common.io.CharStreams;
+import com.google.common.io.Files;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.zip.ZipFile;
 import org.apache.maven.it.VerificationException;
 import org.apache.maven.it.Verifier;
 import org.apache.maven.it.util.ResourceExtractor;
@@ -24,19 +35,22 @@ import org.apache.maven.model.Model;
 import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
 import org.codehaus.plexus.util.FileUtils;
 import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.matchers.JUnitMatchers;
 import org.junit.rules.TemporaryFolder;
-
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.Collections;
 
 public class ProjectTest {
 
+  private static final String DEFAULT_URL = "https://myapi.appspot.com/_ah/api";
+  private static final String DEFAULT_URL_PREFIX = "public static final String DEFAULT_ROOT_URL = ";
+  private static final String DEFAULT_URL_VARIABLE = DEFAULT_URL_PREFIX + "\"https://myapi.appspot.com/_ah/api/\";";
+  private static final String CLIENT_LIB_PATH = "target/client-libs/testApi-v1-java.zip";
+  private static final String DISC_DOC_PATH = "target/discovery-docs/testApi-v1-rest.discovery";
+  private static final String API_JAVA_FILE_PATH = "testApi/src/main/java/com/example/testApi/TestApi.java";
   public static String pluginVersion = null;
 
   @Rule
@@ -64,8 +78,51 @@ public class ProjectTest {
     verifier.executeGoals(
         Arrays.asList("endpoints-framework:clientLibs", "endpoints-framework:discoveryDocs"));
     verifier.verifyErrorFreeLog();
-    verifier.assertFilePresent("target/client-libs/testApi-v1-java.zip");
-    verifier.assertFilePresent("target/discovery-docs/testApi-v1-rest.discovery");
+    verifier.assertFilePresent(CLIENT_LIB_PATH);
+    verifier.assertFilePresent(DISC_DOC_PATH);
+
+    String apiJavaFile = getFileContentsInZip(new File(testDir, CLIENT_LIB_PATH), API_JAVA_FILE_PATH);
+    Assert.assertThat(apiJavaFile, JUnitMatchers.containsString(DEFAULT_URL_VARIABLE));
+
+    String discovery = Files.toString(new File(testDir, DISC_DOC_PATH), Charsets.UTF_8);
+    Assert.assertThat(discovery, JUnitMatchers.containsString(DEFAULT_URL));
+
+
+  }
+
+  @Test
+  public void testApplicationId() throws IOException, VerificationException {
+    File testDir = injectApplicationId(loadProject("/projects/server"), "<application>maven-test</application>");
+
+    Verifier verifier = new Verifier(testDir.getAbsolutePath());
+    verifier.executeGoals(
+        Arrays.asList("endpoints-framework:clientLibs", "endpoints-framework:discoveryDocs"));
+    verifier.verifyErrorFreeLog();
+
+    String apiJavaFile = getFileContentsInZip(new File(testDir, CLIENT_LIB_PATH), API_JAVA_FILE_PATH);
+    Assert.assertThat(apiJavaFile, JUnitMatchers.containsString(DEFAULT_URL_PREFIX + "\"https://maven-test.appspot.com/_ah/api/\";"));
+
+    String discovery = Files.toString(new File(testDir, DISC_DOC_PATH), Charsets.UTF_8);
+    Assert.assertThat(discovery, CoreMatchers.not(JUnitMatchers.containsString(DEFAULT_URL)));
+    Assert.assertThat(discovery, JUnitMatchers.containsString("https://maven-test.appspot.com/_ah/api"));
+  }
+
+  @Test
+  public void testHostnameAddon() throws IOException, VerificationException {
+    File testDir = injectConfiguration(loadProject("/projects/server"),
+        "<configuration><hostname>my.hostname.com</hostname></configuration>");
+
+    Verifier verifier = new Verifier(testDir.getAbsolutePath());
+    verifier.executeGoals(
+        Arrays.asList("endpoints-framework:clientLibs", "endpoints-framework:discoveryDocs"));
+    verifier.verifyErrorFreeLog();
+
+    String apiJavaFile = getFileContentsInZip(new File(testDir, CLIENT_LIB_PATH), API_JAVA_FILE_PATH);
+    Assert.assertThat(apiJavaFile, JUnitMatchers.containsString(DEFAULT_URL_PREFIX + "\"https://my.hostname.com/_ah/api/\";"));
+
+    String discovery = Files.toString(new File(testDir, DISC_DOC_PATH), Charsets.UTF_8);
+    Assert.assertThat(discovery, CoreMatchers.not(JUnitMatchers.containsString(DEFAULT_URL)));
+    Assert.assertThat(discovery, JUnitMatchers.containsString("https://my.hostname.com/_ah/api"));
   }
 
   @Test
@@ -79,7 +136,7 @@ public class ProjectTest {
   }
 
   private File loadProject(String path) throws IOException {
-    File dir = ResourceExtractor.extractResourcePath(ProjectTest.class, path, testRoot.getRoot());
+    File dir = ResourceExtractor.extractResourcePath(ProjectTest.class, path, testRoot.getRoot(), true);
 
     File pom = new File(dir, "pom.xml");
     String pomContents = FileUtils.fileRead(pom);
@@ -87,5 +144,29 @@ public class ProjectTest {
     FileUtils.fileWrite(pom, pomContents);
 
     return dir;
+  }
+
+  // inject a endpoints plugin configuration into the pom.xml
+  private File injectConfiguration(File root, String configuration) throws IOException {
+    File pom = new File(root, "pom.xml");
+    String pomContents = FileUtils.fileRead(pom);
+    pomContents = pomContents.replaceAll("<!--endpoints-plugin-configuration-->", configuration);
+    FileUtils.fileWrite(pom, pomContents);
+    return root;
+  }
+
+  // inject an application tag into the appengine-web.xml
+  private File injectApplicationId(File root, String application) throws IOException {
+    File app = new File(root, "src/main/webapp/WEB-INF/appengine-web.xml");
+    String appContents = FileUtils.fileRead(app);
+    appContents = appContents.replaceAll("<!--application-->", application);
+    FileUtils.fileWrite(app, appContents);
+    return root;
+  }
+
+  private String getFileContentsInZip(File zipFile, String path) throws IOException {
+    ZipFile zip = new ZipFile(zipFile);
+    InputStream is = zip.getInputStream(zip.getEntry(path));
+    return CharStreams.toString(new InputStreamReader(is, Charsets.UTF_8));
   }
 }
