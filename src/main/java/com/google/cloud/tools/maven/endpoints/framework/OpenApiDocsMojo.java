c81a3703302c05d4a4936eab3032190883a5a4f3
Appu Goundan <appu@google.com>
2017-04-27 17:31:05 -0400

    Add open api doc generation goal

---
+++ src/main/java/com/google/cloud/tools/maven/endpoints/framework/OpenApiDocsMojo.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2017 Google Inc. All Right Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.google.cloud.tools.maven.endpoints.framework;
+
+import com.google.api.server.spi.tools.EndpointsTool;
+import com.google.api.server.spi.tools.GetDiscoveryDocAction;
+import com.google.api.server.spi.tools.GetOpenApiDocAction;
+import com.google.common.base.Joiner;
+import com.google.common.base.Strings;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Execute;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.apache.maven.project.MavenProject;
+
+/**
+ * Goal which generates discovery docs
+ */
+@Mojo(name = "openApiDocs", requiresDependencyResolution = ResolutionScope.COMPILE)
+@Execute(phase = LifecyclePhase.PREPARE_PACKAGE)
+public class OpenApiDocsMojo extends AbstractEndpointsWebAppMojo {
+
+  @Parameter(defaultValue = "${project}", readonly = true)
+  private MavenProject project;
+
+  /**
+   * Output directory for discovery docs
+   */
+  @Parameter(defaultValue = "${project.build.directory}/openapi-docs",
+             property = "endpoints.openApiDocDir", required = true)
+  private File openApiDocDir;
+
+  /**
+   * Default hostname of the Endpoint Host.
+   */
+  @Parameter(property = "endpoints.hostname", required = false)
+  private String hostname;
+
+  public void execute() throws MojoExecutionException {
+    try {
+      if (!openApiDocDir.exists()) {
+        if (!openApiDocDir.mkdirs()) {
+          throw new MojoExecutionException(
+              "Failed to create output directory: " + openApiDocDir.getPath());
+        }
+      }
+      String classpath = Joiner.on(File.pathSeparator)
+          .join(project.getCompileClasspathElements(), classesDir);
+
+      List<String> params = new ArrayList<>(Arrays.asList(
+          GetOpenApiDocAction.NAME,
+          "-o", computeOpenApiDocPath(),
+          "-cp", classpath,
+          "-w", webappDir.getPath()));
+      if (!Strings.isNullOrEmpty(hostname)) {
+        params.add("-h");
+        params.add(hostname);
+      }
+      if (serviceClasses != null) {
+        params.addAll(serviceClasses);
+      }
+
+      System.out.print(params);
+      new EndpointsTool().execute(params.toArray(new String[params.size()]));
+
+
+    } catch (Exception e) {
+      throw new MojoExecutionException("Endpoints Tool Error", e);
+    }
+  }
+
+  private String computeOpenApiDocPath() {
+    return new File(openApiDocDir, "openapi.json").getPath();
+  }
+}
--- src/test/java/com/google/cloud/tools/maven/endpoints/framework/ProjectTest.java
+++ src/test/java/com/google/cloud/tools/maven/endpoints/framework/ProjectTest.java
@@ -45,30 +45,35 @@ import org.junit.rules.TemporaryFolder;
 
 public class ProjectTest {
 
-  private static final String DEFAULT_URL = "https://myapi.appspot.com/_ah/api";
+  private static final String DEFAULT_HOSTNAME = "myapi.appspot.com";
+  private static final String DEFAULT_URL = "https://" + DEFAULT_HOSTNAME + "/_ah/api";
   private static final String DEFAULT_URL_PREFIX = "public static final String DEFAULT_ROOT_URL = ";
   private static final String DEFAULT_URL_VARIABLE = DEFAULT_URL_PREFIX + "\"https://myapi.appspot.com/_ah/api/\";";
   private static final String CLIENT_LIB_PATH = "target/client-libs/testApi-v1-java.zip";
   private static final String DISC_DOC_PATH = "target/discovery-docs/testApi-v1-rest.discovery";
+  private static final String OPEN_API_DOC_PATH = "target/openapi-docs/openapi.json";
   private static final String API_JAVA_FILE_PATH = "testApi/src/main/java/com/example/testApi/TestApi.java";
-  public static String pluginVersion = null;
+
+  private final String pluginVersion;
 
   @Rule
   public TemporaryFolder testRoot = new TemporaryFolder();
 
+  public ProjectTest() throws IOException, XmlPullParserException {
+    MavenXpp3Reader reader = new MavenXpp3Reader();
+    Model model = reader.read(new FileReader("pom.xml"));
+    pluginVersion = model.getVersion();
+  }
+
   @BeforeClass
-  public static void setUp() throws VerificationException, IOException, XmlPullParserException {
+  public static void setUp() throws VerificationException {
     Verifier verifier = new Verifier(".", true);
     // clean is causing some issues with appveyor builds
     verifier.setAutoclean(false);
     verifier.addCliOption("-DskipTests");
     verifier.executeGoal("install");
-
-    MavenXpp3Reader reader = new MavenXpp3Reader();
-    Model model = reader.read(new FileReader("pom.xml"));
-
-    pluginVersion = model.getVersion();
   }
+
 
   @Test
   public void testServerArtifactCreation() throws IOException, VerificationException {
@@ -76,7 +81,7 @@ public class ProjectTest {
 
     Verifier verifier = new Verifier(testDir.getAbsolutePath());
     verifier.executeGoals(
-        Arrays.asList("endpoints-framework:clientLibs", "endpoints-framework:discoveryDocs"));
+        Arrays.asList("endpoints-framework:clientLibs", "endpoints-framework:discoveryDocs", "endpoints-framework:openApiDocs"));
     verifier.verifyErrorFreeLog();
     verifier.assertFilePresent(CLIENT_LIB_PATH);
     verifier.assertFilePresent(DISC_DOC_PATH);
@@ -87,7 +92,8 @@ public class ProjectTest {
     String discovery = Files.toString(new File(testDir, DISC_DOC_PATH), Charsets.UTF_8);
     Assert.assertThat(discovery, JUnitMatchers.containsString(DEFAULT_URL));
 
-
+    String openapi = Files.toString(new File(testDir, OPEN_API_DOC_PATH), Charsets.UTF_8);
+    Assert.assertThat(openapi, JUnitMatchers.containsString(DEFAULT_HOSTNAME));
   }
 
   @Test
@@ -96,7 +102,7 @@ public class ProjectTest {
 
     Verifier verifier = new Verifier(testDir.getAbsolutePath());
     verifier.executeGoals(
-        Arrays.asList("endpoints-framework:clientLibs", "endpoints-framework:discoveryDocs"));
+        Arrays.asList("endpoints-framework:clientLibs", "endpoints-framework:discoveryDocs", "endpoints-framework:openApiDocs"));
     verifier.verifyErrorFreeLog();
 
     String apiJavaFile = getFileContentsInZip(new File(testDir, CLIENT_LIB_PATH), API_JAVA_FILE_PATH);
@@ -105,6 +111,10 @@ public class ProjectTest {
     String discovery = Files.toString(new File(testDir, DISC_DOC_PATH), Charsets.UTF_8);
     Assert.assertThat(discovery, CoreMatchers.not(JUnitMatchers.containsString(DEFAULT_URL)));
     Assert.assertThat(discovery, JUnitMatchers.containsString("https://maven-test.appspot.com/_ah/api"));
+
+    String openapi = Files.toString(new File(testDir, OPEN_API_DOC_PATH), Charsets.UTF_8);
+    Assert.assertThat(discovery, CoreMatchers.not(JUnitMatchers.containsString(DEFAULT_HOSTNAME)));
+    Assert.assertThat(openapi, JUnitMatchers.containsString("maven-test.appspot.com"));
   }
 
   @Test
@@ -114,7 +124,7 @@ public class ProjectTest {
 
     Verifier verifier = new Verifier(testDir.getAbsolutePath());
     verifier.executeGoals(
-        Arrays.asList("endpoints-framework:clientLibs", "endpoints-framework:discoveryDocs"));
+        Arrays.asList("endpoints-framework:clientLibs", "endpoints-framework:discoveryDocs", "endpoints-framework:openApiDocs"));
     verifier.verifyErrorFreeLog();
 
     String apiJavaFile = getFileContentsInZip(new File(testDir, CLIENT_LIB_PATH), API_JAVA_FILE_PATH);
@@ -123,6 +133,10 @@ public class ProjectTest {
     String discovery = Files.toString(new File(testDir, DISC_DOC_PATH), Charsets.UTF_8);
     Assert.assertThat(discovery, CoreMatchers.not(JUnitMatchers.containsString(DEFAULT_URL)));
     Assert.assertThat(discovery, JUnitMatchers.containsString("https://my.hostname.com/_ah/api"));
+
+    String openapi = Files.toString(new File(testDir, OPEN_API_DOC_PATH), Charsets.UTF_8);
+    Assert.assertThat(discovery, CoreMatchers.not(JUnitMatchers.containsString(DEFAULT_HOSTNAME)));
+    Assert.assertThat(openapi, JUnitMatchers.containsString("my.hostname.com"));
   }
 
   @Test
