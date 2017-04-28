b22ff1896a22e4c545f9dc756852f83cb84dfa76
Appu Goundan <appu@google.com>
2017-04-28 16:54:39 -0400

    Clean up and separate out tests

---
+++ src/test/java/com/google/cloud/tools/maven/endpoints/framework/ClientLibsMojoTest.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2017 Google Inc. All Right Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.google.cloud.tools.maven.endpoints.framework;
+
+
+import com.google.common.base.Charsets;
+import com.google.common.io.CharStreams;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.zip.ZipFile;
+import org.apache.maven.it.VerificationException;
+import org.apache.maven.it.Verifier;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.matchers.JUnitMatchers;
+import org.junit.rules.TemporaryFolder;
+
+/** Test for ClientLibsMojo. */
+public class ClientLibsMojoTest {
+
+  private static final String DEFAULT_HOSTNAME = "myapi.appspot.com";
+  private static final String DEFAULT_URL = "https://" + DEFAULT_HOSTNAME + "/_ah/api/";
+  private static final String DEFAULT_URL_PREFIX = "public static final String DEFAULT_ROOT_URL = ";
+  private static final String DEFAULT_URL_VARIABLE = DEFAULT_URL_PREFIX + "\"" + DEFAULT_URL + "\";";
+  private static final String CLIENT_LIB_PATH = "target/client-libs/testApi-v1-java.zip";
+  private static final String API_JAVA_FILE_PATH = "testApi/src/main/java/com/example/testApi/TestApi.java";
+
+  @Rule
+  public TemporaryFolder tmpDir = new TemporaryFolder();
+
+  @Test
+  public void testDefault() throws IOException, VerificationException, XmlPullParserException {
+    File testDir = new TestProject(tmpDir.getRoot(), "/projects/server").build();
+
+    Verifier verifier = new Verifier(testDir.getAbsolutePath());
+    verifier.executeGoal("endpoints-framework:clientLibs");
+    verifier.verifyErrorFreeLog();
+    verifier.assertFilePresent(CLIENT_LIB_PATH);
+
+    String apiJavaFile = getFileContentsInZip(new File(testDir, CLIENT_LIB_PATH), API_JAVA_FILE_PATH);
+    Assert.assertThat(apiJavaFile, JUnitMatchers.containsString(DEFAULT_URL_VARIABLE));
+  }
+
+  @Test
+  public void testApplicationId() throws IOException, VerificationException, XmlPullParserException {
+    File testDir = new TestProject(tmpDir.getRoot(), "/projects/server").applicationId("maven-test").build();
+
+    Verifier verifier = new Verifier(testDir.getAbsolutePath());
+    verifier.executeGoal("endpoints-framework:clientLibs");
+    verifier.verifyErrorFreeLog();
+
+    String apiJavaFile = getFileContentsInZip(new File(testDir, CLIENT_LIB_PATH), API_JAVA_FILE_PATH);
+    Assert.assertThat(apiJavaFile, JUnitMatchers.containsString(DEFAULT_URL_PREFIX + "\"https://maven-test.appspot.com/_ah/api/\";"));
+
+  }
+
+  @Test
+  public void testHostname() throws IOException, VerificationException, XmlPullParserException {
+    File testDir = new TestProject(tmpDir.getRoot(), "/projects/server")
+        .configuration("<configuration><hostname>my.hostname.com</hostname></configuration>")
+        .build();
+
+    Verifier verifier = new Verifier(testDir.getAbsolutePath());
+    verifier.executeGoal("endpoints-framework:clientLibs");
+    verifier.verifyErrorFreeLog();
+
+    String apiJavaFile = getFileContentsInZip(new File(testDir, CLIENT_LIB_PATH), API_JAVA_FILE_PATH);
+    Assert.assertThat(apiJavaFile, JUnitMatchers.containsString(DEFAULT_URL_PREFIX + "\"https://my.hostname.com/_ah/api/\";"));
+
+  }
+
+  private String getFileContentsInZip(File zipFile, String path) throws IOException {
+    ZipFile zip = new ZipFile(zipFile);
+    InputStream is = zip.getInputStream(zip.getEntry(path));
+    return CharStreams.toString(new InputStreamReader(is, Charsets.UTF_8));
+  }
+}
---
+++ src/test/java/com/google/cloud/tools/maven/endpoints/framework/OpenApiDocsMojoTest.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2017 Google Inc. All Right Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.google.cloud.tools.maven.endpoints.framework;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Files;
+import java.io.File;
+import java.io.IOException;
+import org.apache.maven.it.VerificationException;
+import org.apache.maven.it.Verifier;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.matchers.JUnitMatchers;
+import org.junit.rules.TemporaryFolder;
+
+public class OpenApiDocsMojoTest {
+
+  private static final String DEFAULT_HOSTNAME = "myapi.appspot.com";
+  private static final String OPEN_API_DOC_PATH = "target/openapi-docs/openapi.json";
+
+  @Rule
+  public TemporaryFolder tmpDir = new TemporaryFolder();
+
+  @Test
+  public void testDefault() throws IOException, VerificationException, XmlPullParserException {
+    File testDir = new TestProject(tmpDir.getRoot(), "/projects/server").build();
+
+    Verifier verifier = new Verifier(testDir.getAbsolutePath());
+    verifier.executeGoal("endpoints-framework:openApiDocs");
+    verifier.verifyErrorFreeLog();
+    verifier.assertFilePresent(OPEN_API_DOC_PATH);
+
+    String openapi = Files.toString(new File(testDir, OPEN_API_DOC_PATH), Charsets.UTF_8);
+    Assert.assertThat(openapi, JUnitMatchers.containsString(DEFAULT_HOSTNAME));
+  }
+
+  @Test
+  public void testApplicationId() throws IOException, VerificationException, XmlPullParserException {
+    File testDir = new TestProject(tmpDir.getRoot(), "/projects/server").applicationId("maven-test").build();
+
+    Verifier verifier = new Verifier(testDir.getAbsolutePath());
+    verifier.executeGoal("endpoints-framework:openApiDocs");
+    verifier.verifyErrorFreeLog();
+
+    String openapi = Files.toString(new File(testDir, OPEN_API_DOC_PATH), Charsets.UTF_8);
+    Assert.assertThat(openapi, CoreMatchers.not(JUnitMatchers.containsString(DEFAULT_HOSTNAME)));
+    Assert.assertThat(openapi, JUnitMatchers.containsString("maven-test.appspot.com"));
+
+  }
+
+  @Test
+  public void testHostname() throws IOException, VerificationException, XmlPullParserException {
+    File testDir = new TestProject(tmpDir.getRoot(), "/projects/server")
+        .configuration("<configuration><hostname>my.hostname.com</hostname></configuration>")
+        .build();
+
+    Verifier verifier = new Verifier(testDir.getAbsolutePath());
+    verifier.executeGoal("endpoints-framework:openApiDocs");
+    verifier.verifyErrorFreeLog();
+
+    String openapi = Files.toString(new File(testDir, OPEN_API_DOC_PATH), Charsets.UTF_8);
+    Assert.assertThat(openapi, CoreMatchers.not(JUnitMatchers.containsString(DEFAULT_HOSTNAME)));
+    Assert.assertThat(openapi, JUnitMatchers.containsString("my.hostname.com"));
+  }
+
+}
+
