c0c061a1396e1009153633bf78bfd0913b52ffe4
Aaron Signorelli <aarons88@gmail.com>
2017-09-06 19:08:48 +0100

    Adding basePath as a parameter that can be passed in through the POM  (#35)
    * Exposing basePath as a param that can be passed in through the POM

--- src/main/java/com/google/cloud/tools/maven/endpoints/framework/OpenApiDocsMojo.java
+++ src/main/java/com/google/cloud/tools/maven/endpoints/framework/OpenApiDocsMojo.java
@@ -18,7 +18,6 @@
 package com.google.cloud.tools.maven.endpoints.framework;
 
 import com.google.api.server.spi.tools.EndpointsTool;
-import com.google.api.server.spi.tools.GetDiscoveryDocAction;
 import com.google.api.server.spi.tools.GetOpenApiDocAction;
 import com.google.common.base.Joiner;
 import com.google.common.base.Strings;
@@ -34,9 +33,7 @@ import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.apache.maven.project.MavenProject;
 
-/**
- * Goal which generates openapi docs
- */
+/** Goal which generates openapi docs */
 @Mojo(name = "openApiDocs", requiresDependencyResolution = ResolutionScope.COMPILE)
 @Execute(phase = LifecyclePhase.PREPARE_PACKAGE)
 public class OpenApiDocsMojo extends AbstractEndpointsWebAppMojo {
@@ -44,18 +41,21 @@ public class OpenApiDocsMojo extends AbstractEndpointsWebAppMojo {
   @Parameter(defaultValue = "${project}", readonly = true)
   private MavenProject project;
 
-  /**
-   * Output directory for openapi docs
-   */
-  @Parameter(defaultValue = "${project.build.directory}/openapi-docs",
-             property = "endpoints.openApiDocDir", required = true)
+  /** Output directory for openapi docs */
+  @Parameter(
+    defaultValue = "${project.build.directory}/openapi-docs",
+    property = "endpoints.openApiDocDir",
+    required = true
+  )
   private File openApiDocDir;
 
-  /**
-   * Default hostname of the endpoint host.
-   */
+  /** Default hostname of the endpoint host. */
   @Parameter(property = "endpoints.hostname", required = false)
   private String hostname;
+
+  /** Default basePath of the endpoint host. */
+  @Parameter(property = "endpoints.basePath", required = false)
+  private String basePath;
 
   public void execute() throws MojoExecutionException {
     try {
@@ -66,14 +66,23 @@ public class OpenApiDocsMojo extends AbstractEndpointsWebAppMojo {
       String classpath = Joiner.on(File.pathSeparator).join(project.getRuntimeClasspathElements());
       classpath += File.pathSeparator + classesDir;
 
-      List<String> params = new ArrayList<>(Arrays.asList(
-          GetOpenApiDocAction.NAME,
-          "-o", computeOpenApiDocPath(),
-          "-cp", classpath,
-          "-w", webappDir.getAbsolutePath()));
+      List<String> params =
+          new ArrayList<>(
+              Arrays.asList(
+                  GetOpenApiDocAction.NAME,
+                  "-o",
+                  computeOpenApiDocPath(),
+                  "-cp",
+                  classpath,
+                  "-w",
+                  webappDir.getAbsolutePath()));
       if (!Strings.isNullOrEmpty(hostname)) {
         params.add("-h");
         params.add(hostname);
+      }
+      if (!Strings.isNullOrEmpty(basePath)) {
+        params.add("-p");
+        params.add(basePath);
       }
       if (serviceClasses != null) {
         params.addAll(serviceClasses);
@@ -81,7 +90,6 @@ public class OpenApiDocsMojo extends AbstractEndpointsWebAppMojo {
 
       getLog().info("Endpoints Tool params : " + params.toString());
       new EndpointsTool().execute(params.toArray(new String[params.size()]));
-
 
     } catch (Exception e) {
       throw new MojoExecutionException("Endpoints Tool Error", e);
